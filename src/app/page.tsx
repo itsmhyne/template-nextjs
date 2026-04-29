341220f03b71c0c7ef53a53452866beaee551e9e
mhamdany <mhamdanydev@gmail.com>
2026-04-29 20:22:09 +0700

    edited page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,70 @@
-import Image from "next/image";
+// import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
 
-export default function Home() {
+import { AppleIcon, CloudDownload, LockIcon, ServerIcon } from "lucide-react";
+
+const features = [
+  {
+    name: "Clone to developent",
+    description:
+      "Silakan clone website ini jika tertarik menggunakannya!.(tidak ada penjelesan detail tetang kodingan ini, PAHAMI SENDIRI)",
+    icon: CloudDownload,
+  },
+  {
+    name: "Note",
+    description:
+      "Sebenearnya ini untuk projek pribadi, kalau anda berminat silakan di clone!",
+    icon: AppleIcon,
+  },
+  {
+    name: "Database",
+    description:
+      "Database menggunakan PostgreSQL dikominasi dengan Prisma. Kenapa menggunakan PostgreSQL?, biar bisa migrasi ke Supabase 🤣",
+    icon: ServerIcon,
+  },
+];
+
+export default function Example() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>ANJ</h1>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="overflow-hidden h-screen flex flex-col justify-center items-center">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pt-4 lg:pr-8">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base/7 font-semibold text-indigo-400">
+                Template NextJs with Bun 🧅
+              </h2>
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-black sm:text-5xl">
+                A better template<sup className="text-xs">kali</sup>
+              </p>
+              <p className="mt-6 text-lg/8 text-black-300">
+                Sebuah template autentikasi yang dibangun dengan NextJs, Prisma,
+                PostgreSQL, dan state manajemen Zustand.
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-black-400 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-black">
+                      <feature.icon
+                        aria-hidden="true"
+                        className="absolute top-1 left-1 size-5 text-indigo-400"
+                      />
+                      {feature.name}
+                    </dt>{" "}
+                    <dd className="inline">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+          <img
+            alt="Product screenshot"
+            src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
+            width={2432}
+            height={1442}
+            className="w-3xl max-w-none rounded-xl shadow-xl ring-1 ring-white/10 sm:w-228 md:-ml-4 lg:-ml-0"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
